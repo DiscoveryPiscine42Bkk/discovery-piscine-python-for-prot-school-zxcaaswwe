929d6da1377fb4d582d24f2a921c875bd5f93c31
zxcaaswwe <41140@prot.ac.th>
2025-03-11 10:04:11 +0700

    Add files via upload

---
+++ cell01/ex1.3/whatsyourname.py
@@ -0,0 +1,8 @@
+# Ask for the user's first name
+first_name = input("Hey, what's your first name? : ")
+
+# Ask for the user's last name
+last_name = input("And your last name? : ")
+
+# Display the full name
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
