0f750485f675709ff399f9cc59970058ee2600dc
zxcaaswwe <41140@prot.ac.th>
2025-03-11 13:09:23 +0700

    Add files via upload

---
+++ cell04/ex4.2/calculator.py
@@ -0,0 +1,13 @@
+    #!/usr/bin/env python3
+
+# Prompt user for two numbers
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+print("Thank you!")
+
+# Perform calculations and display results
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
